08f7d776eec168cb07257cab0fce923d6e39b8d5
Joshua Dong <jdong42@gmail.com>
2013-09-29 01:28:41 -0500

    Added basic move verification.

--- chess.py
+++ chess.py
@@ -33,19 +33,38 @@ data = {
     (6, 5): Fore.WHITE+'\xe2\x99\x9f',
     (6, 6): Fore.WHITE+'\xe2\x99\x9f',
     (6, 7): Fore.WHITE+'\xe2\x99\x9f',
+    #user input
+    (-1, 0): '00 00',
     #move color
     (-1, -1): 1,
     #status
     (-2, -2): 0
 }
 
-def validate(action):
-    (data.update({(-2, -2): 1}), data.update({(-1, -1): (data[(-1, -1)]-1)**2})) if int(data[(8-int(action.split(' ')[0][1]), ord(action.split(' ')[0][0])-ord('a'))][3])/7 == data[(-1, -1)] else ''
-    return action
+def go(v):
+    for c in v:
+        if 'a' <= c <= 'z':
+            print c
+    time.sleep(3)
+    return True
 
 while 1:
-    #set -3, -2 to 0
+    #Set move valid status (data[(-2, -2)]) to 0.
+    #Display board.
+    #Display who's turn it is based on data[(-1, -1)].
     print '' if data.update({(-3, -3): 0}) else ''+'' if data.update({(-2, -2): 0}) else ''+'\x1B[2J\x1B[H'+(''.join([(('\x1b[49m\n')*(len%2)+'\x1b[49m\n')[:6] if hgt==8 else Back.MAGENTA+(data[(len, hgt)] if (len, hgt) in data.keys() else ' ') if (len+hgt)%2 else Back.CYAN+(data[(len, hgt)] if (len, hgt) in data.keys() else ' ') for len in xrange(8) for hgt in xrange(9)]))+'\n'+(("White"*data[(-1, -1)]+'Black')[:5])+"'s turn."
-    action = validate(raw_input(Fore.WHITE+">"))
-    data[(8-int(action.split(' ')[1][1]), ord(action.split(' ')[1][0])-ord('a')) if data[(-2, -2)] else (-2, -2)] = data.pop((8-int(action.split(' ')[0][1]), ord(action.split(' ')[0][0])-ord('a')) if data[(-2, -2)] else (-3, -3))
-
+    #Puts the user input into data[(-1, 0)].
+    #If the move is invalid, do not set status (data[(-2, -2)]) to 1.
+    (((data.update({(-2, -2): 1}),
+    data.update({(-1, -1): (data[(-1, -1)]-1)**2})) if\
+        int(data[(8-int(data[(-1, 0)].split(' ')[0][1]),
+                 ord(data[(-1, 0)].split(' ')[0][0])-ord('a'))][3])/7 == data[(-1, -1)] else\
+            '') if\
+                data[(-1, 0)].count('')-1 == 5 and\
+                '1' <= data[(-1, 0)][1] <= '8' and '1' <= data[(-1, 0)][4] <= '8' and\
+                'a' <= data[(-1, 0)][0] <= 'h' and 'a' <= data[(-1, 0)][3] <= 'h' else\
+                    raw_input('Invalid input.')) if\
+                        data.update({(-1, 0): raw_input(Fore.WHITE+">>")}) or True else\
+                            ''
+    #Apply move on data if valid
+    data[(8-int(data[(-1, 0)].split(' ')[1][1]), ord(data[(-1, 0)].split(' ')[1][0])-ord('a')) if data[(-2, -2)] else (-2, -2)] = data.pop((8-int(data[(-1, 0)].split(' ')[0][1]), ord(data[(-1, 0)].split(' ')[0][0])-ord('a')) if data[(-2, -2)] else (-3, -3))
