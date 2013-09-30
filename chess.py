7a9c65164dbd237ec0f575f0663d174d5bbad703
Joshua Dong <jdong42@gmail.com>
2013-09-29 19:59:19 -0500

    reformatted lines for git, added new spacing for board.

--- chess.py
+++ chess.py
@@ -46,19 +46,35 @@ while 1:
     #Set move valid status (data[(-2, -2)]) to 0.
     #Display board.
     #Display who's turn it is based on data[(-1, -1)].
-    print '' if data.update({(-3, -3): 0}) else ''+'' if data.update({(-2, -2): 0}) else ''+'\x1B[2J\x1B[H'+(''.join([(('\x1b[49m\n')*(len%2)+'\x1b[49m\n')[:6] if hgt==8 else Back.MAGENTA+(data[(len, hgt)] if (len, hgt) in data.keys() else ' ') if (len+hgt)%2 else Back.CYAN+(data[(len, hgt)] if (len, hgt) in data.keys() else ' ') for len in xrange(8) for hgt in xrange(9)]))+'\n'+(("White"*data[(-1, -1)]+'Black')[:5])+"'s turn."
+    print '' if data.update({(-3, -3): 0}) else\
+            ''+'' if data.update({(-2, -2): 0}) else\
+    '\x1B[2J\x1B[H'+(''.join([(('\x1b[49m\n')*(lng%2)+'\x1b[49m\n')[:6] if\
+        hgt==8 else\
+            Back.MAGENTA+(data[(lng, hgt)]+' ' if\
+                (lng, hgt) in data.keys() else "  ") if\
+                        (lng+hgt)%2 else\
+            Back.CYAN+(data[(lng, hgt)]+' ' if\
+                (lng, hgt) in data.keys() else "  ")
+        for lng in xrange(8) for hgt in xrange(9)]))+\
+    '\n'+(("White"*data[(-1, -1)]+'Black')[:5])+"'s turn."
     #Puts the user input into data[(-1, 0)].
     #If the move is invalid, do not set status (data[(-2, -2)]) to 1.
     (((data.update({(-2, -2): 1}),
-    data.update({(-1, -1): (data[(-1, -1)]-1)**2})) if\
+       data.update({(-1, -1): (data[(-1, -1)]-1)**2})) if\
         int(data[(8-int(data[(-1, 0)].split(' ')[0][1]),
-                 ord(data[(-1, 0)].split(' ')[0][0])-ord('a'))][3])/7 == data[(-1, -1)] else\
-            '') if\
+                 ord(data[(-1, 0)].split(' ')[0][0])-ord('a'))][3])/7 ==\
+      data[(-1, -1)] else '') if\
                 data[(-1, 0)].count('')-1 == 5 and\
-                '1' <= data[(-1, 0)][1] <= '8' and '1' <= data[(-1, 0)][4] <= '8' and\
-                'a' <= data[(-1, 0)][0] <= 'h' and 'a' <= data[(-1, 0)][3] <= 'h' else\
-                    raw_input('Invalid input.')) if\
-                        data.update({(-1, 0): raw_input(Fore.WHITE+">>")}) or True else\
-                            ''
+                '1' <= data[(-1, 0)][1] <= '8' and\
+                '1' <= data[(-1, 0)][4] <= '8' and\
+                'a' <= data[(-1, 0)][0] <= 'h' and\
+                'a' <= data[(-1, 0)][3] <= 'h' else\
+     raw_input('Invalid input.')) if\
+    data.update({(-1, 0): raw_input(Fore.WHITE+">>")}) or True else ''
     #Apply move on data if valid
-    data[(8-int(data[(-1, 0)].split(' ')[1][1]), ord(data[(-1, 0)].split(' ')[1][0])-ord('a')) if data[(-2, -2)] else (-2, -2)] = data.pop((8-int(data[(-1, 0)].split(' ')[0][1]), ord(data[(-1, 0)].split(' ')[0][0])-ord('a')) if data[(-2, -2)] else (-3, -3))
+    data[(8-int(data[(-1, 0)].split(' ')[1][1]),
+          ord(data[(-1, 0)].split(' ')[1][0])-ord('a')) if\
+        data[(-2, -2)] else (-2, -2)] =\
+    data.pop((8-int(data[(-1, 0)].split(' ')[0][1]),
+              ord(data[(-1, 0)].split(' ')[0][0])-ord('a')) if\
+        data[(-2, -2)] else (-3, -3))
